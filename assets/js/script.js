d22527765b4970941f455b3bcc9e2b74ff5cc547
dombeesley <quigley_beesley@hotmail.com>
2020-02-16 17:42:13 +0000

    edit landmarks information

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,4 +3,41 @@
       $(".bktitle").click(function(){
         $(".bkinfo").toggle();
     });
+
+    $(".bajtitle").click(function(){
+        $(".bajinfo").toggle();
+    });
+
+    $(".frametitle").click(function(){
+        $(".frameinfo").toggle();
+    });
+
+    $(".skititle").click(function(){
+        $(".skiinfo").toggle();
+    });
+
+    $(".avenuetitle").click(function(){
+        $(".avenueinfo").toggle();
+    });
+
+    $(".lamertitle").click(function(){
+        $(".lamerinfo").toggle();
+    });
+
+    $(".aquatitle").click(function(){
+        $(".aquainfo").toggle();
+    });
+
+    $(".miracletitle").click(function(){
+        $(".miracleinfo").toggle();
+    });
+
+    $(".operatitle").click(function(){
+        $(".operainfo").toggle();
+    });
+
+    $(".imgtitle").click(function(){
+        $(".imginfo").toggle();
+    });
+
   });
